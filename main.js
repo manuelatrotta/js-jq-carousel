e94b255278b365ff521c2073e3f46ac4d188b6a9
manuelatrotta <manuelat88@hotmail.it>
2020-01-17 16:02:02 +0100

    prova con alert per vedere se viene effettuato l'effetto al click

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //obiettivo: far scorrere le immagini al click sulle freccette laterali e allo stesso tempo deve andare avanti il cerchio colorandosi.
 
 //step 1 creiamo una funzione che con azione 'click' agisca sulle classi 'prev' e 'next'
-$(document).ready(
+$(document) .ready(
   function() {
     $('.next').click(
       function() {
@@ -9,12 +9,14 @@ $(document).ready(
       }
     );
 
-$(document).ready(
-  function() {
     $('.prev').click(
       function() {
-        clickNext();
+        clickPrev();
       }
     );
   }
 );
+// funzione con clickNext in cui si avrà lo scorrimento delle immagini
+function clickNext() {
+  alert('click Next');
+}
