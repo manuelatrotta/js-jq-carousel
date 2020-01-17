b975d00cae38a22b2385a7f73d208e18c6053161
manuelatrotta <manuelat88@hotmail.it>
2020-01-17 15:45:56 +0100

    inizio impostazione risoluzione

---
+++ main.js
@@ -0,0 +1,5 @@
+//obiettivo: far scorrere le immagini al click sulle freccette laterali e allo stesso tempo deve andare avanti il cerchio colorandosi.
+
+//step 1 creiamo una funzione che con azione 'click' agisca sulle classi 'prev' e 'next'
+
+  alert('ciao');
