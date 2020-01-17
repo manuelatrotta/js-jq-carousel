3a1def756df30b4b310fe1b04a00dd67e39e4162
manuelatrotta <manuelat88@hotmail.it>
2020-01-17 15:52:16 +0100

    funzione con azione click su class next e .prev

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 //obiettivo: far scorrere le immagini al click sulle freccette laterali e allo stesso tempo deve andare avanti il cerchio colorandosi.
 
 //step 1 creiamo una funzione che con azione 'click' agisca sulle classi 'prev' e 'next'
+$(document).ready(
+  function() {
+    $('.next').click(
+      function() {
+        clickNext();
+      }
+    );
 
-  alert('ciao');
+$(document).ready(
+  function() {
+    $('.prev').click(
+      function() {
+        clickNext();
+      }
+    );
+  }
+);
